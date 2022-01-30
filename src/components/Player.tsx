3f3ead0f7f5604b72ba68d778e0f61a617f06946
Nick Manning <hello@manninglabs.com>
2022-01-30 12:31:17 -0500

    Video player

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,27 +1,38 @@
-// source: https://codesandbox.io/s/hidden-frost-mpdjj?file=/src/App.tsx:156-292
-
-// import { useEffect, useRef } from "preact/hooks";
 import { useEffect, useRef } from "react";
+// import "./styles.css";
 
 import "plyr-react/dist/plyr.css";
 import Hls from "hls.js";
 import Plyr, { APITypes, PlyrProps, PlyrInstance } from "plyr-react";
 
-const MyComponent = (src) => {
-  console.log(src.src.src);
+const videoSrc: Plyr.SourceInfo = {
+  type: "video",
+  sources: [
+    {
+      src: "yWtFb9LJs3o",
+      provider: "youtube",
+    },
+  ],
+};
+
+export default function Player() {
   const ref = useRef<APITypes>(null);
+
   useEffect(() => {
     const loadVideo = async () => {
       const video = document.getElementById("plyr") as HTMLVideoElement;
+
       var hls = new Hls();
-      hls.loadSource(src.src.src); // FIXME wtf
+      hls.loadSource(
+        "https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8"
+      ); // "https://cdn.plyr.io/static/blank.mp4");
       hls.attachMedia(video);
       // @ts-ignore
       ref.current!.plyr.media = video;
 
-      hls.on(Hls.Events.MANIFEST_PARSED, function () {
-        (ref.current!.plyr as PlyrInstance).play();
-      });
+      // hls.on(Hls.Events.MANIFEST_PARSED, function () {
+      //   (ref.current!.plyr as PlyrInstance).play();
+      // });
     };
     loadVideo();
   });
@@ -34,18 +45,14 @@ const MyComponent = (src) => {
       ref={ref}
     />
   );
-};
-
-export default function Player(src) {
-  const supported = Hls.isSupported();
-
-  return (
-    <div>
-      {supported ? (
-        <MyComponent src={src} />
-      ) : (
-        "HLS is not supported in your browser"
-      )}
-    </div>
-  );
 }
+
+// export default function App() {
+//   const supported = Hls.isSupported();
+
+//   return (
+//     <div>
+//       {supported ? <MyComponent /> : "HLS is not supported in your browser"}
+//     </div>
+//   );
+// }
