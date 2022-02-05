d13e88b2b38a296aaa2fb1dd83c7547e8962f2e9
Nick Manning <hello@manninglabs.com>
2022-02-05 14:34:38 -0500

    flexbox layout and custom theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,13 @@ module.exports = {
   ],
   // plugins: [require("@themesberg/flowbite/plugin")],
   darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4E44AF", // medium purple
+        secondary: "#1A1640", // dark purple
+        accent: "#ADA5FF", // light purple
+      },
+    },
+  },
 };
