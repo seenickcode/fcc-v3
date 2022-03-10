4e2c2d7749c8f13d23a2e1898e19e02b1ce59ee6
Nick Manning <hello@manninglabs.com>
2022-03-10 03:29:41 -0500

    localStorage for dark mode

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -1,78 +1,65 @@
 import { Component } from "preact";
 
 interface State {
-  title: string;
+  on: boolean;
 }
 
 export default class DarkModeToggle extends Component<{}, State> {
   constructor(props) {
     super(props);
-    this.state = { title: "off" };
+
+    // determine if dark mode should be on
+    let isOn = false;
+    if (localStorage) {
+      // get local storage value
+      const val = localStorage.getItem("dark-mode");
+
+      if (val) {
+        // set it
+        isOn = val === "1";
+      } else {
+        // if not set, see if the user prefers dark mode
+        isOn = window.matchMedia("(prefers-color-scheme: dark)").matches;
+      }
+    }
+
+    // set document class to dark if needed
+    this.applyState(isOn);
+
+    // set state accordingly
+    this.state = { on: isOn };
   }
+
+  applyState = (on: boolean) => {
+    if (on) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+
+    if (localStorage) {
+      localStorage.setItem("dark-mode", on ? "1" : "");
+    }
+  };
 
   toggle = (e) => {
     e.preventDefault();
-    const isOn = this.state.title == "on";
-    if (isOn) {
-      document.documentElement.classList.remove("dark");
-    } else {
-      document.documentElement.classList.add("dark");
-    }
+
+    const newOn = !this.state.on;
+
+    this.applyState(newOn);
+
+    // update local state
     this.setState({
-      title: isOn ? "off" : "on",
+      on: newOn,
     });
-
-    // var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
-    // var themeToggleLightIcon = document.getElementById(
-    //   "theme-toggle-light-icon"
-    // );
-
-    // // Change the icons inside the button based on previous settings
-    // if (
-    //   localStorage.getItem("color-theme") === "dark" ||
-    //   (!("color-theme" in localStorage) &&
-    //     window.matchMedia("(prefers-color-scheme: dark)").matches)
-    // ) {
-    //   themeToggleLightIcon.classList.remove("hidden");
-    // } else {
-    //   themeToggleDarkIcon.classList.remove("hidden");
-    // }
-
-    // var themeToggleBtn = document.getElementById("theme-toggle");
-
-    // themeToggleBtn.addEventListener("click", function () {
-    //   // toggle icons inside button
-    //   themeToggleDarkIcon.classList.toggle("hidden");
-    //   themeToggleLightIcon.classList.toggle("hidden");
-
-    //   // if set via local storage previously
-    //   if (localStorage.getItem("color-theme")) {
-    //     if (localStorage.getItem("color-theme") === "light") {
-    //       document.documentElement.classList.add("dark");
-    //       localStorage.setItem("color-theme", "dark");
-    //     } else {
-    //       document.documentElement.classList.remove("dark");
-    //       localStorage.setItem("color-theme", "light");
-    //     }
-
-    //     // if NOT set via local storage previously
-    //   } else {
-    //     if (document.documentElement.classList.contains("dark")) {
-    //       document.documentElement.classList.remove("dark");
-    //       localStorage.setItem("color-theme", "light");
-    //     } else {
-    //       document.documentElement.classList.add("dark");
-    //       localStorage.setItem("color-theme", "dark");
-    //     }
-    //   }
-    // });
   };
 
   render() {
     return (
       <p>
         <a href="#" onClick={this.toggle}>
-          {this.state.title}
+          Dark Mode {this.state.on ? "On" : "Off"}
         </a>
       </p>
     );
