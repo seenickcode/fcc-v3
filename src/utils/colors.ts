acc0c1abce90059cd062e1ed8b87085cddf54ad1
Nick Manning <hello@manninglabs.com>
2022-03-11 10:56:08 -0500

    Footer content

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,6 +1,7 @@
 const primary = "bg-primary text-black dark:bg-secondary dark:text-primary";
 const secondary = "bg-white text-black dark:bg-primary dark:text-white";
 const accent = "bg-accent text-white dark:bg-secondary dark:text-primary";
+const tertiary = "bg-secondary text-white dark:bg-secondary dark:text-primary";
 
 export function themeColors(themeName: string): string {
   return themeName == "primary"
@@ -9,5 +10,7 @@ export function themeColors(themeName: string): string {
     ? secondary
     : themeName == "accent"
     ? accent
+    : themeName == "tertiary"
+    ? tertiary
     : "";
 }
