adce29930240f7ca7aa9eaf12f233294fcc23a69
Nick Manning <hello@manninglabs.com>
2022-03-12 20:00:43 -0500

    About page.

--- cms/schemas/lesson.js
+++ cms/schemas/lesson.js
@@ -1,71 +1,71 @@
 export default {
-    name: 'lesson',
-    title: 'Lesson',
-    type: 'document',
-    fields: [
-        {
-            name: 'slug',
-            title: 'Slug',
-            type: 'slug',
-            validation: Rule => Rule.required(),
-        },
-        {
-            name: 'title',
-            title: 'Title',
-            type: 'string',
-            validation: Rule => Rule.required(),
-        },
-        {
-            name: 'subtitle',
-            title: 'Subtitle',
-            type: 'string',
-        },
-        {
-            name: 'video_id',
-            title: 'Video ID',
-            type: 'string',
-            validation: Rule => Rule.required(),
-        },
-        {
-            name: 'show_notes',
-            title: 'Show Notes',
-            type: 'markdown',
-        },
-        {
-            name: 'public',
-            title: 'Is Public',
-            type: 'boolean',
-        },
-        {
-            name: 'duration_mins',
-            title: 'Duration (Minutes part)',
-            type: 'number',
-        },
-        {
-            name: 'duration_secs',
-            title: 'Duration (Seconds part)',
-            type: 'number',
-        },
-        {
-            name: 'thumbnail_url',
-            title: 'Thumbnail URL',
-            type: 'string',
-        },
-        {
-            name: 'seq_number',
-            title: 'Sequence Number',
-            type: 'number',
-        },
-        {
-            name: 'publishedAt',
-            title: 'Published at',
-            type: 'datetime',
-        },
-    ],
-
-    preview: {
-        select: {
-            title: 'title',
-        },
+  name: "lesson",
+  title: "Lesson",
+  type: "document",
+  fields: [
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      validation: (Rule) => Rule.required(),
     },
-}
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "subtitle",
+      title: "Subtitle",
+      type: "string",
+    },
+    {
+      name: "video_id",
+      title: "Video ID",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "show_notes",
+      title: "Show Notes",
+      type: "markdown",
+    },
+    {
+      name: "public",
+      title: "Is Public",
+      type: "boolean",
+    },
+    {
+      name: "duration_mins",
+      title: "Duration (Minutes part)",
+      type: "number",
+    },
+    {
+      name: "duration_secs",
+      title: "Duration (Seconds part)",
+      type: "number",
+    },
+    {
+      name: "thumbnail_url",
+      title: "Thumbnail URL",
+      type: "string",
+    },
+    {
+      name: "seq_number",
+      title: "Sequence Number",
+      type: "number",
+    },
+    {
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
+    },
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+    },
+  },
+};
